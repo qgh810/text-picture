e74d65cebd13d614e9e6eba68d06f9e34b23065e
qiuguohui <qiuguohui@bytedance.com>
2020-12-13 10:54:36 +0800

    feat: 完成文字化改造

---
+++ genUnit.js
@@ -0,0 +1,9 @@
+// function init() {
+//     genCanvas()
+// }
+
+// function genCanvas() {
+//     const canvas = document.getElementById('text-canvas')
+//     const ctx = canvas.getContext('2d');
+//     ctx.f
+// }
--- index.js
+++ index.js
@@ -8,82 +8,87 @@
 //     ctx.font = '35px Microsoft YaHei';
 //     ctx.fillText('hello world, hello world, hello world', 50, 50);
 // }
-const WIDTH = 2000;
-const HEIGHT = 2000;
-let ctx = null
-let image = null;
-let imageData = null;
-const grayColorCount = 8; // 不可以大于256
+const WIDTH = 200;
+const HEIGHT = 200;
+const mixColorCount = 4 // 去除边界极端颜色数量
+const colorCount = 10
+const grayColorCount = colorCount + mixColorCount; // 不可以大于256
 const colorStep = parseInt(256 / grayColorCount);
 const colors = genGrayColor(grayColorCount)
+const colorIndexs = []
+
+let imageData = null;
 
 window.onload = function () {
-  initData()
-  drawImage()
-  getImageData()
-  handleImageData()
-  putColorData()
+  const imageCanvasCtx = initImageData()
+  getColorIndexs(imageCanvasCtx)
+  print()
 }
 
-function initData() {
-  const canvas = document.getElementById('canvas');
+function initImageData() {
+  const imageCanvas = document.getElementById('image-canvas');
   // const textCanvas = document.getElementById('text');
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-  ctx = canvas.getContext('2d');
+  imageCanvas.width = WIDTH;
+  imageCanvas.height = HEIGHT;
+  const ctx = imageCanvas.getContext('2d');
   ctx.imageSmoothingEnabled = false;
-  image = document.getElementById('image')
-}
-
-function drawImage() {
+  const image = document.getElementById('image');
   ctx.drawImage(image, 0, 0, WIDTH, HEIGHT);
+  return ctx;
 }
 
-function getImageData () {
-  imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
-}
-
-function handleImageData() {
+function getColorIndexs (imageCanvasCtx) {
+  imageData = imageCanvasCtx.getImageData(0, 0, WIDTH, HEIGHT);
   const picLength = WIDTH * HEIGHT
   for (var i = 0; i < picLength * 4; i += 4) {
-
-    var myRed = imageData.data[i];
-
-    var myGreen = imageData.data[i + 1];
-
-    var myBlue = imageData.data[i + 2];
-
-    myGray = getGrayColor(myRed, myGreen, myBlue);
-    // console.log(myGray)
-
-    imageData.data[i] = myGray;
-    imageData.data[i + 1] = myGray;
-    imageData.data[i + 2] = myGray;
+    var r = imageData.data[i];
+    var g = imageData.data[i + 1];
+    var b = imageData.data[i + 2];
+    const myGray =  parseInt((r + g + b) / 3);
+    let colorIndex = parseInt(myGray / colorStep)
+    colorIndexs.push(colorIndex)
   }
 }
 
-function putColorData() {
-  ctx.putImageData(imageData, 0, 0);
+function print() {
+  const resultCanvas = document.getElementById('result-canvas');
+  const size = 14
+  resultCanvas.width = WIDTH * size
+  resultCanvas.height = HEIGHT * size
+  const resultCanvasCtx = resultCanvas.getContext('2d');
+
+  colorIndexs.forEach((colorIndex, index) => {
+    const y = parseInt(index / WIDTH) * size;
+    const x = (index % WIDTH) * size;
+    printItem(colorIndex, x, y, resultCanvasCtx, size)
+  });
 }
 
-function getGrayColor(r, g, b) {
-  const gray =  parseInt((r + g + b) / 3) + 20;
-  const colorIndex = parseInt(gray / colorStep)
-  const color = colors[colorIndex];
-  // if (color === undefined) {
-  //   throw new Error('color is null')
-  // }
-  return color || 0;
-  // const newGray = gray
+function printItem (colorIndex, x, y, ctx, size) {
+  const grayColorNum = colors[colorIndex];
+  const color = `rgb(${grayColorNum}, ${grayColorNum}, ${grayColorNum})`
+  ctx.fillStyle = color;
+  // ctx.fillRect(x, y, size, size);
+  ctx.font = size + 'px Microsoft YaHei';
+  ctx.fillText('妮', x, y);
 }
 
 function genGrayColor(grayColorCount) {
   const colors = []
   for (let index = 0; index < grayColorCount + 1; index++) {
     let color = parseInt(colorStep * index)
-    if (color >= 180) color = 256
-    if (color <= 80) color = 0
+    if (color >= 256) color = 256
     colors.push(color) 
   }
+
+  // 抹除边界的颜色粒度，减少颜色
+  colors.forEach((color, index) => {
+    if (index >= colors.length - mixColorCount) {
+      colors[index] = 256
+    } else if (index < mixColorCount) {
+      colors[index] = 0
+    }
+  })
+
   return colors;
 }
